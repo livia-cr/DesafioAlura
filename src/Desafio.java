c271570ea41af2b258adc6cae90c5a5a62021e39
Lívia Cardoso Rodrigues <livia.cardoso@cdlbh.com.br>
2025-11-13 12:33:15 -0300

    Cria classe Desafio com dados do cliente

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Desafio {
+    public static void main(String[] args) {
+        //Dados do Cliente
+        String nome = "Bernardo Souza";
+        String conta = "Corrente";
+        double saldo = 2500.00;
+
+        System.out.println("***********************");
+        System.out.println("Dados iniciais do cliente:");
+        System.out.println("\nNome: "+nome);
+        System.out.println("Tipo conta: "+conta);
+        System.out.printf("Saldo inicial: %.2f%n",saldo);
+        System.out.println("\n***********************");
+    }
 }
