12d395f7bed23dd6395e9655bb68a52ead726794
Lívia Cardoso Rodrigues <livia.cardoso@cdlbh.com.br>
2025-11-14 11:25:00 -0300

    Adiciona menu de opções e leitura de entrada com Scanner

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Desafio {
     public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
         //Dados do Cliente
         String nome = "Bernardo Souza";
         String conta = "Corrente";
         double saldo = 2500.00;
+        int opcao = 0;
 
         System.out.println("***********************");
         System.out.println("Dados iniciais do cliente:");
@@ -11,5 +15,20 @@ public class Desafio {
         System.out.println("Tipo conta: "+conta);
         System.out.printf("Saldo inicial: %.2f%n",saldo);
         System.out.println("\n***********************");
+
+        //Menu de opções
+        String menu = """
+                \n** Opções:
+                1- Consutar saldos
+                2- Receber Valor
+                3- Transferir valor
+                4- Sair
+                
+                Digite sua opção:
+                """;
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leia.nextInt();
+        }
     }
 }
